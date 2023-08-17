17efe18eee95c8cefa974b37cdbd673d9d7c513f
Albina Yevtushenko <109302599+albinayevtushenko@users.noreply.github.com>
2023-08-17 22:19:47 +0300

    add changes

--- Helpers/HttpError.js
+++ Helpers/HttpError.js
@@ -1,9 +1,15 @@
-const HttpError = (status, message) => {
-  const error = new Error(message);
-  error.status = status;
-
-  return error;
+const messages = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
 };
 
-// експортуємо
+const HttpError = (status, message = messages[status]) => {
+  const err = new Error(message);
+  err.status = status;
+  return err;
+};
+
 module.exports = HttpError;
---
+++ decorators/controllerWrapper.js
@@ -0,0 +1,12 @@
+const controllerWrapper = (ctrl) => {
+  const wrapper = async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+  return wrapper;
+};
+
+module.exports = controllerWrapper;
