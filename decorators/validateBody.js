7a6d87320a58f3eaba982b82cc9b2789a563e6f1
Albina Yevtushenko <109302599+albinayevtushenko@users.noreply.github.com>
2023-08-18 22:54:48 +0300

    add new cchanges

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -2,10 +2,16 @@ const { HttpError } = require("../Helpers");
 
 const validateBody = (schema) => {
   const validate = (req, res, next) => {
+    const body = req.body;
+    if (Object.keys(body).length === 0) {
+      next(HttpError(400, "missing fields"));
+    }
+
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, "missing required name field"));
+      next(HttpError(400, error.message));
     }
+
     next();
   };
   return validate;
--- schemas/contactsSchema.js
+++ schemas/contactsSchema.js
@@ -1,9 +1,14 @@
 const Joi = require("joi");
 
 const contactsSchema = Joi.object({
-  name: Joi.string().min(2).max(255).required(),
-  email: Joi.string().min(4).max(255).required().email(),
-  phone: Joi.string().min(4).max(20).required(),
+  name: Joi.string().required().error(new Error("missing required name field")),
+  email: Joi.string()
+    .required()
+    .email()
+    .error(new Error("missing required email field")),
+  phone: Joi.string()
+    .required()
+    .error(new Error("missing required phone field")),
 });
 
 module.exports = contactsSchema;
