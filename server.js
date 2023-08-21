785a2b7434cabf3a7457118a9e5635f17eaaf379
Albina Yevtushenko <109302599+albinayevtushenko@users.noreply.github.com>
2023-08-21 20:27:45 +0300

    add new changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const { DB_HOST, PORT = 3000 } = process.env;
-
 mongoose.set("strictQuery", true);
 
+const { DB_URI, PORT = 3000 } = process.env;
+
 mongoose
-  .connect(DB_HOST)
+  .connect(DB_URI)
   .then(app.listen(PORT, () => console.log("Database connection successful")))
   .catch((error) => {
     console.log(error.message);
