2dfaff7412bb353ee9ee0c5137cd0e2946238c5a
Albina Yevtushenko <109302599+albinayevtushenko@users.noreply.github.com>
2023-08-22 13:46:41 +0300

    update

--- decorators/index.js
+++ decorators/index.js
@@ -1,7 +1,9 @@
 const controllerWrapper = require("./controllerWrapper");
 const validateBody = require("./validateBody");
+const validation = require("./validation");
 
 module.exports = {
   controllerWrapper,
   validateBody,
+  validation,
 };
